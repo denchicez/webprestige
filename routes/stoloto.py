6e90b1431a4647e01004ed6b463be9f8187c54e5
denchicez <denchicez@gmail.com>
2023-02-28 14:53:58 +0300

    more stable version

--- routes/stoloto.py
+++ routes/stoloto.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from handlers.stoloto import *
@@ -7,16 +9,32 @@ stoloto = APIRouter()
 
 @stoloto.get("/stoloto/top3")
 async def get_top3() -> HTMLResponse:
-    table = await stoloto_top3_table()
+    while True:
+        try:
+            table = await stoloto_top3_table()
+            break
+        except:
+            await asyncio.sleep(10)
     return HTMLResponse(content=convert_arr_table_to_html(table), status_code=200)
 
 
 @stoloto.get("/stoloto/keno2")
 async def get_keno2() -> HTMLResponse:
-    table = await stoloto_keno2_table()
+    while True:
+        try:
+            table = await stoloto_keno2_table()
+            break
+        except:
+            await asyncio.sleep(10)
     return HTMLResponse(content=convert_arr_table_to_html(table), status_code=200)
+
 
 @stoloto.get("/stoloto/bigloto")
 async def get_keno2() -> HTMLResponse:
-    table = await stoloto_bigloto_table()
+    while True:
+        try:
+            table = await stoloto_bigloto_table()
+            break
+        except:
+            await asyncio.sleep(10)
     return HTMLResponse(content=convert_arr_table_to_html(table), status_code=200)
